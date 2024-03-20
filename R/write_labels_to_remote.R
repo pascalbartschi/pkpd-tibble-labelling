432022735668d4bbc54f22117a8588fd21ec5d91
Baertschi <bartscp1@emea.roche.com>
2024-03-20 09:45:10 +0100

    added output label file

--- R/write_labels_to_remote.R
+++ R/write_labels_to_remote.R
@@ -1,13 +1,14 @@
 # This functions writes the labels to the output folder
 
-write_labels_to_remote <- function(filename = "output/all_variables2labels.csv"){
+write_labels_to_remote <- function(directory = "output", filename = "all_variables2labels.csv"){
   labels <- read_labels()
   
+  if (!(dir.exists(directory))) {dir.create(directory)}
   # Enclose labels in double quotes
   labels_with_quotes <- sprintf('"%s"', labels)
   
   write.csv(data.frame(name = names(labels), 
                        labels = labels_with_quotes),
-            file = filename, 
+            file = file.path(directory, filename), 
             row.names = FALSE)
 }
