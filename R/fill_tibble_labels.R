89f78c34e436950d3c8cf968200e23a651f97fa2
pascalbartschi <paesce.26@gmail.com>
2024-03-22 11:33:14 +0100

    bug fix in add_tibble_lables.R

--- R/fill_tibble_labels.R
+++ R/fill_tibble_labels.R
@@ -3,7 +3,7 @@
 fill_tibble_labels <- function(tbble, mode = "remote") {
   # read in labels and assign labels to columns names
   if (mode == "remote"){
-    labels <- read.csv("https://raw.githubusercontent.com/pascalbartschi/pkpd-tibble-labelling/main/output/all_variables2labels.csv")
+    labels <- read.csv("https://raw.githubusercontent.com/pascalbartschi/pkpd-tibble-labelling/main/output/all_variables2labels.csv") %>%  pull(2, 1) # converts df to names list
   } else {
     labels <- read_labels(path = "data")
   }
